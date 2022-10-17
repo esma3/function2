20ad0a88dfa4763ecbd102c6dddc2f055dc7db58
esma3 <semababayeva9898@gmail.com>
2022-10-18 03:20:47 +0400

    first commit

---
+++ js/main.js
@@ -0,0 +1,61 @@
+
+
+// function goster(){
+//    document.getElementById("satici").innerHTML="+9940504432823"
+// }
+// function rengini_deyis(){
+//    document.getElementById('satici').style.color="red"
+// }
+// function rengini_deyisi(){
+//    document.getElementById('satici').style.color="green"
+// }
+// function baxildi(){
+//    document.getElementById("satici").innerHTML="nomreye baxildi"
+// }
+
+// function revan(){
+//     document.body.style.backgroundColor="yellow"
+
+//     document.getElementsByTagName("body")[0].style.backgroundColor="green"
+// }
+
+
+
+
+// function masin_sec(){
+//     var select = document.getElementById('select')
+//     var sekil = document.getElementById('sekil')
+
+//     // sekil.src = "https://motor.ru/imgs/2022/03/03/10/5274800/79931cca9770ca2bee4d28cccfc066a60e2af017.jpg"
+//     console.log()
+//     sekil.src="img/" + select.value + ".png"
+// }
+
+
+function masin_sec(){
+        var select = document.getElementById('select')
+        var sekil = document.getElementById('sekil')
+    
+if(select.value=="bmw"){
+    sekil.src="https://motor.ru/imgs/2022/03/03/10/5274800/79931cca9770ca2bee4d28cccfc066a60e2af017.jpg"
+}else if(select.value=="KIA"){
+    sekil.src="https://upload.wikimedia.org/wikipedia/commons/0/0f/00_kia_k5_3_0.jpg"
+}
+}
+
+
+
+
+// function rengi_deyis(e){
+//     e.style.background="blue"
+// }
+
+// function rengi_qaytar(deyanet){
+//     deyanet.style.background="green"
+// }
+// function rengi_redlesdir(esmira){
+//     esmira.style.border="5px solid red"
+//     esmira.style.background ="transparent"
+// }
+// function divi_sil(e){
+//     e.style.display="none"}
